a189d3282d75afd3c41801c38315047b433437c6
Dominic May <jack.thatch@gmail.com>
2014-09-24 22:57:10 +0800

    Cleanup

--- src/parser.c
+++ src/parser.c
@@ -5,13 +5,10 @@
 parser* create_parser() {
     parser* ps = malloc(sizeof(*ps));
 
-    ps->lx = create_lexer();
-
     return ps;
 }
 
 
 void free_parser(parser* ps) {
-    free_lexer(ps->lx);
     free(ps);
 }
--- src/parser.h
+++ src/parser.h
@@ -1,10 +1,8 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "src/lexer.h"
 
 typedef struct {
-    lexer* lx;
 } parser;
 
 parser* create_parser();
