9ddfa7dad3b04b1564c0d934b253eb1f419ac4b4
Dominic May <jack.thatch@gmail.com>
2014-09-25 18:42:21 +0800

    use getopt

--- src/main.c
+++ src/main.c
@@ -3,36 +3,42 @@
 #include "src/sea.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Must provide an input file\n");
-        return -1;
+    // parse arguments
+    while((opt = getopt(argc, argv, "d:")) != -1) {
+        switch(opt) {
+            case 'd': {
+                extern int yydebug;
+                yydebug = 1;
+            }
+            default: {
+                fprintf(stderr, "Usage: %s [-d] input_filename\n", argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
     }
 
+    if (optind >= argc) {
+        fprintf(stderr, "Expected argument after options\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // use arguments
+    char* filename = argv[optind];
     FILE* input_file;
-    int is_stdin = strcmp(argv[1], "-") == 0;
+    int is_stdin = strcmp(filename, "-") == 0;
 
-    if (is_stdin) {
-        input_file = stdin;
-    } else {
-        input_file = fopen(argv[1], "rb");
-    }
-
+    input_file = is_stdin ? stdin : fopen(filename, "rb");
     if (!input_file) {
         perror("An error occured whilst reading from the input file");
         return -1;
     }
 
-    if (argc > 2) {
-        if (strcmp(argv[2], "-d") == 0) {
-            extern int yydebug;
-            yydebug = 1;
-        }
-    }
-
+    // main part of program
     sea* s = create_sea();
-    execute_file(s, input_file);
+    int stat = execute_file(s, input_file);
     free_sea(s);
 
+    // cleanup
     if (!is_stdin) {
         if (ferror(input_file)) {
             fprintf(stderr, "An error occured during reading from the file\n");
@@ -40,5 +46,10 @@ int main(int argc, char* argv[]) {
         fclose(input_file);
     }
 
-    return 0;
+    if (stat != 0) {
+        fprintf(stderr, "An occured during parsing\n");
+        return stat;
+    } else {
+        return 0;
+    }
 }
