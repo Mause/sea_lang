d11ae20f5db84ffc1db3e858b3ade83acdb713fd
Dominic May <jack.thatch@gmail.com>
2014-09-26 17:30:24 +0800

    eval attempt

---
+++ src/eval.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "src/ast/ast.h"
+#include "src/bool.h"
+#include "src/compile.h"
+#include "src/dict/dict.h"
+#include "src/object/object.h"
+#include "src/object/none.h"
+#include "src/stack.h"
+
+int run_function(ASTNode* func, ASTNode* program, callstack* stack);
+
+
+int eval(ASTNode* ast) {
+    assert(ast->type == NODE_MANYNODES);
+
+    callstack* stack = compile(ast);
+
+    ASTNode* main = dict_get(stack->top->variables, "main");
+    if (main == NULL) return -1;
+
+    run_function(
+        main,
+        ast,
+        stack
+    );
+
+    return 0;
+}
+
+
+SeaObject* get_variable(char* name, stack_frame* stack) {
+    SeaObject* obj = dict_get(stack->variables, name);
+
+    if (obj == NULL) {
+        return get_variable(name, stack->previous);
+    } else {
+        return obj;
+    }
+}
+
+int run_function(ASTNode* func, ASTNode* program, callstack* cs) {
+    assert(func->type == NODE_FUNCTION);
+
+    stack_frame* cur_frame = stack_push(cs);
+    int pc;
+
+    many_nodes* body = func->func->body;
+    assert(body);
+    assert(body->num_nodes > 0);
+
+    printf("(Looping)\n");
+    while (TRUE) {
+        if (pc == body->num_nodes) break;
+
+        ASTNode* current = body->nodes[pc];
+        printf("%s\n", repr(current->type));
+
+        switch(current->type) {
+        case NODE_DECLARATION: {
+            if (current->declare->expr == NULL) {
+                dict_set(
+                    cur_frame->variables,
+                    current->declare->ident,
+                    SeaNone
+                );
+            } else {
+                fprintf(stderr, "var's with values not yet suppored\n");
+            }
+            break;
+        }
+        case NODE_ASSIGNMENT: {
+            void* val = get_variable(
+                current->assign->ident,
+                cs->top
+            );
+
+            if (val == NULL) {
+                fprintf(stderr, "No such variable as %s\n", current->assign->ident);
+            }
+            break;
+        }
+        case NODE_FUNCTION_CALL: {
+            // run_function(current->call->function, program, stack);
+            break;
+        }
+        default: {
+            fprintf(stderr, "Unknown; %s\n", repr(current->type));
+            assert(0);
+        }
+        }
+
+        pc++;
+    }
+    dict_repr(cs->top->variables);
+}
