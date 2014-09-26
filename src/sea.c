7852b2c8e218c40ebf934cb006432996fdc60307
Dominic May <jack.thatch@gmail.com>
2014-09-26 17:32:27 +0800

    Connect the guts

--- src/sea.c
+++ src/sea.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include "src/sea.h"
+#include "build/grammar.h"
 
+#include "src/ast/ast.h"
 
 sea* create_sea() {
     sea* s = malloc(sizeof(*s));
@@ -16,8 +19,29 @@ void free_sea(sea* s) {
 }
 
 extern FILE* yyin;
+extern void* res;
+
 int execute_file(sea* s, FILE* file) {
     yyin = file;
 
-    return yyparse();
+    int return_val = yyparse();
+
+    if (return_val != 0)  return return_val;
+
+    ASTNode* ast = res;
+    printf("\n---------------------------\n");
+    render_ast(ast, -1);
+    printf("\n---------------------------\n");
+
+    // free_ast(ast);
+
+    int stat = eval(ast);
+
+    if (stat != 0) {
+        printf("An error occured during evaluation\n");
+    }
+
+    // assert(0);
+
+    return 0;
 }
